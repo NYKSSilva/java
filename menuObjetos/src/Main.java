c84e1a7d3cdfc94b378688f1d3735920374c3bc4
NYKSSilva <nicollysantossilva0508@gmail.com>
2026-02-09 10:42:43 -0300

    validacao

--- menuObjetos/src/Main.java
+++ menuObjetos/src/Main.java
@@ -1,13 +1,16 @@
 import java.util.ArrayList;
 
 public class Main {
+
     private static ArrayList<Aluno> listaAlunos = new ArrayList<>();
     private static ArrayList<Turma> listaTurmas = new ArrayList<>();
     public static void main(String[] args) {
      menuPrincipal();
     }
+
+
     public static void menuPrincipal(){
-        System.out.println("==== Secretaria ====");
+        System.out.println("\n==== Secretaria ====");
         System.out.println("1 - Alunos.");
         System.out.println("2 - Turmas.");
         System.out.println("3 - Sair.");
@@ -25,14 +28,13 @@ public class Main {
                 System.exit(0);
             default:
                 System.out.println("Opção invalida! Tente novamente!");
-
-
+                menuPrincipal();
         }
     }
 
     // alunos
     private static void menuAlunos() {
-        System.out.println("==== Alunos ====");
+        System.out.println("\n==== Alunos ====");
         System.out.println("1 - Listar Alunos.");
         System.out.println("2 - Cadastrar Alunos.");
         System.out.println("3 - Atualizar Alunos.");
@@ -43,6 +45,7 @@ public class Main {
         switch (opcao){
             case "1":
                 listarAlunos();
+                menuAlunos();
                 break;
             case "2":
                 CadastrarAluno();
@@ -58,11 +61,16 @@ public class Main {
                 break;
             default:
                 System.out.println("Opção invalida! Tente novamente!");
+                menuAlunos();
         }
     }
 
 
     private static void listarAlunos() {
+        if(listaAlunos.isEmpty()){
+            System.out.println("Não há alunos cadastrados.");
+            return;
+        }
         for (Aluno a : listaAlunos){
             System.out.println(a);
         }
@@ -79,7 +87,7 @@ public class Main {
 
     // turma
     private static void menuTurma() {
-        System.out.println("==== Turmas ====");
+        System.out.println("\n==== Turmas ====");
         System.out.println("1 - Listar Turmas.");
         System.out.println("2 - Cadastrar Turma.");
         System.out.println("3 - Atualizar Turma.");
@@ -90,6 +98,7 @@ public class Main {
         switch (opcao){
             case "1":
                 listarTurmas();
+                menuTurma();
                 break;
             case "2":
                 CadastrarTurmas();
@@ -105,17 +114,63 @@ public class Main {
                 break;
             default:
                 System.out.println("Opção invalida! Tente novamente!");
+                menuTurma();
         }
     }
 
     private static void listarTurmas() {
 
+        if (listaTurmas.isEmpty()) {
+            System.out.println("Não há turmas cadastradas.");
+            return;
+        }
         for (Turma t : listaTurmas){
             System.out.println(t);
         }
     }
 
     private static void CadastrarTurmas() {
+        Periodo periodo = ValidarPeriodo();
+        String curso = Leitura.dados("Digite o curso:");
+        if(!isCharacther(curso))
+            System.out.println("nome de curso invalido, digite apenas textos!");
+
+        String sigla = Leitura.dados("Digite a sigla:");
+        menuTurma();
+    }
+
+    private static boolean isCharacther(String curso) {
+        String textoSemNumero = curso.replaceAll("\\d", " ");
+        return  !curso.isBlank() && curso.equals((textoSemNumero));
+    }
+
+    private static Periodo ValidarPeriodo(){
+        Periodo periodo;
+        String opcaoPeriodo = Leitura.dados("""
+                Digite o numero do periodo escolhido:
+                1 - Matutino
+                2 - Vespertino
+                3 - Noturno
+                4 - Integral""");
+
+        switch (opcaoPeriodo){
+            case "1":
+                return Periodo.MATUTINO;
+
+            case "2":
+                return Periodo.VESPERTINO;
+
+            case "3":
+                return Periodo.NOTURNO;
+
+            case "4":
+                return Periodo.INTEGRAL;
+
+            default:
+                System.out.println("Opção invalida, tente novamente");
+                return ValidarPeriodo();
+        }
+
     }
 
     private static void AtualizarTurmas() {
